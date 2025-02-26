60432f9c9df557df5c023375ca5b99183037f2af
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2025-02-26 07:41:27 +0000

    Refactor, add tests

--- src/taskwait/__init__.py
+++ src/taskwait/__init__.py
@@ -1,24 +1,25 @@
 import math
 import time
 from abc import ABC, abstractmethod
+from contextlib import contextmanager
 from dataclasses import dataclass
 
 
 class Task(ABC):
-    status_waiting: list[str]
-    status_running: list[str]
+    status_waiting: set[str]
+    status_running: set[str]
 
     @abstractmethod
     def status(self) -> str:
-        pass
+        pass  # pragma: no cover
 
     @abstractmethod
     def log(self) -> list[str] | None:
-        return None
+        return None  # pragma: no cover
 
     @abstractmethod
     def has_log(self) -> bool:
-        pass
+        pass  # pragma: no cover
 
 
 @dataclass
@@ -35,11 +36,13 @@ class RunningTask:
         task: Task,
         *,
         show_log: bool = True,
+        progress: bool = True,
         poll: int = 1,
         timeout: float | None = None,
     ):
         self._task = task
         self._show_log = show_log and task.has_log()
+        self._progress = progress and not self._show_log
         self._poll = poll
         self._skip = 0
         self._t0 = time.time()
@@ -53,40 +56,74 @@ class RunningTask:
         return Result(self._status, self._t0, time.time())
 
     def _wait_to_start(self) -> None:
-        while self._status in self._task.status_waiting:
-            self._status = self._task.status()
+        display = self._show_log or self._progress
+        with very_simple_progress("Waiting", display=display) as p:
+            while self._status in self._task.status_waiting:
+                p()
+                self._status = self._task_status()
 
     def _wait_to_finish(self) -> None:
-        while self._status in self._task.status_running:
-            self._show_new_log()
-            self._status = self._task.status()
+        with very_simple_progress("Running", display=self._progress) as p:
+            while self._status in self._task.status_running:
+                p()
+                self._show_new_log()
+                self._status = self._task_status()
         self._show_new_log()
 
     def _task_status(self) -> str:
-        if self._last_time is not None:
-            now = time.time()
-            if now > self._time_end:
-                raise TimeoutError()
-            wait = self._poll - (now - self._last_time)
-            if wait > 0:
-                time.sleep(wait)
-        self._last_time = time.time()
+        self._last_time = _delay(self._last_time, self._poll, self._time_end)
         return self._task.status()
 
     def _show_new_log(self) -> None:
         if self._show_log:
-            curr = self._task.log()
-            if curr and len(curr) > self._skip:
-                print("\n".join(curr[self._skip :]))
-                self._skip = len(curr)
+            self._skip = _show_new_log(self._skip, self._task.log())
 
 
 def taskwait(
     task: Task,
     *,
     show_log: bool = True,
+    progress: bool = True,
     poll: int = 1,
     timeout: float | None = None,
 ) -> Result:
-    t = RunningTask(task, show_log=show_log, poll=poll, timeout=timeout)
+    t = RunningTask(
+        task, show_log=show_log, progress=progress, poll=poll, timeout=timeout
+    )
     return t.run()
+
+
+def _delay(prev, poll, end) -> float:
+    now = time.time()
+    if prev is None:
+        return now
+    if now > end:
+        raise TimeoutError()
+    wait = poll - (now - prev)
+    if wait > 0:
+        time.sleep(wait)
+    return now
+
+
+def _show_new_log(skip: int, value: list[str] | None) -> int:
+    if not value or len(value) <= skip:
+        return skip
+    print("\n".join(value[skip:]))
+    return len(value)
+
+
+# Small stub that we'll swap in for something better later.
+@contextmanager
+def very_simple_progress(start, end="OK", *, display: bool = True):
+    if display:
+        print(start, end="", flush=True)
+
+    def fn():
+        if display:
+            print(".", end="", flush=True)
+
+    try:
+        yield fn
+    finally:
+        if display:
+            print(end, flush=True)
--- tests/test_taskwait.py
+++ tests/test_taskwait.py
@@ -1,4 +1,10 @@
-from taskwait import Result, Task, taskwait
+import math
+import time
+from unittest import mock
+
+import pytest
+
+from taskwait import Result, Task, _delay, taskwait
 
 
 class ExampleTask(Task):
@@ -6,15 +12,16 @@ class ExampleTask(Task):
         self.plan = list(reversed(plan))
         self.n = 0
         self.show_logs = show_logs
-        self.status_waiting = ["created", "submitted"]
-        self.status_running = ["running", "finishing"]
+        self.status_waiting = {"created", "submitted"}
+        self.status_running = {"running", "finishing"}
+        self.logs: list[str] = []
 
     def status(self):
         return self.plan.pop()
 
-    def log(self) -> None:
-        self.n += 1
-        print(f"Log entry {self.n}")
+    def log(self) -> list[str]:
+        self.logs.append(f"Log entry {len(self.logs) + 1}")
+        return self.logs
 
     def has_log(self) -> bool:
         return self.show_logs
@@ -34,4 +41,39 @@ def test_can_print_logs(capsys):
     result = taskwait(task, poll=0)
     assert result.status == "done"
     out = capsys.readouterr().out
-    assert out == "".join([f"Log entry {i + 1}\n" for i in range(3)])
+    assert out == (
+        "Waiting..OK\n" + "".join([f"Log entry {i + 1}\n" for i in range(3)])
+    )
+
+
+def test_can_timeout():
+    prev = time.time() - 100
+    end = time.time() - 1
+    with pytest.raises(TimeoutError):
+        _delay(prev, 0, end)
+
+
+def test_delay_if_required(mocker):
+    t = 1000000
+    poll = 10
+    end = math.inf
+    mock_sleep = mock.MagicMock()
+    mock_time = mock.MagicMock(return_value=t)
+    mocker.patch("time.time", mock_time)
+    mocker.patch("time.sleep", mock_sleep)
+
+    # We don't call sleep if we've never called the function before:
+    assert _delay(None, poll, end) == t
+    assert mock_time.call_count == 1
+    assert mock_sleep.call_count == 0
+
+    # We don't call sleep if we waited more than poll seconds since last time
+    assert _delay(t - 20, poll, end) == t
+    assert mock_time.call_count == 2
+    assert mock_sleep.call_count == 0
+
+    # We don't call sleep if we waited more than poll seconds since last time
+    assert _delay(t - 3, poll, end) == t
+    assert mock_time.call_count == 3
+    assert mock_sleep.call_count == 1
+    assert mock_sleep.mock_calls[0] == mock.call(7)
